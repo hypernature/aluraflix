628a0e408f5c204ce4cb7e23278a4f1a472e8c2a
hypernature <hypernature@gmail.com>
2026-07-19 10:56:15 -0600

    Fix case-sensitive imports for Linux build

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Header from './components/Header';
+import Header from './components/header';
 import GlobalStyle from './globalstyle';
 import Page404 from './Page404'
 import NuevoVideo from './NuevoVideo';
 import NuevaCategoria from './NuevaCategoria';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 
 function App() {
   return (
